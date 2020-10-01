3b3c7cfe9cc91c6f9ed17328fa2ee65c4788753f
DonnyLVu <donnyvu897@gmail.com>
2020-09-30 17:06:55 -0700

    save this code for now

--- app.js
+++ app.js
@@ -1,5 +1,118 @@
+import { gameFun } from '../utils.js';
+
 // import functions and grab DOM elements
 
+const button = document.querySelector('#submit-button');
+const winSpan = document.querySelector('#win-span');
+const loseSpan = document.querySelector('#lose-span');
+const perSpan = document.querySelector('#percentage-span');
+const resultSpan = document.querySelector('#result-span');
+const drawSpan = document.querySelector('#draw-span');
 // initialize state
 
+let wins = 0;
+let total = 0;
+let lose = 0;
+let draws = 0;
+
 // set event listeners to update state and DOM
+
+button.addEventListener('click', () => {
+    const computerChoice = gameFun();
+    const radioButton = document.querySelector('checked');
+    const userChoice = radioButton.value;
+
+    //draws
+    if (userChoice === 'rock' && computerChoice === 'rock'){
+        draws++;
+        total++;
+
+        resultSpan.textContent = 'Draw!';
+        winSpan.textContent = wins;
+        loseSpan.textContent = lose;
+        drawSpan.textContent = draws;
+        perSpan.textContent = `${(wins / total) * 100}%`;
+    } else if (userChoice === 'paper' && computerChoice === 'paper'){
+        draws++;
+        total++;
+
+        resultSpan.textContent = 'Draw!';
+        winSpan.textContent = wins;
+        loseSpan.textContent = lose;
+        drawSpan.textContent = draws;
+        perSpan.textContent = `${(wins / total) * 100}%`;
+    } else if (userChoice === 'scissor' && computerChoice === 'scissor'){
+        draws++;
+        total++;
+
+        resultSpan.textContent = 'Draw!';
+        winSpan.textContent = wins;
+        loseSpan.textContent = lose;
+        drawSpan.textContent = draws;
+        perSpan.textContent = `${(wins / total) * 100}%`;
+    }
+    //wins
+    if (userChoice === 'rock' && computerChoice === 'scissor'){
+        wins++;
+        total++;
+
+        resultSpan.textContent = 'Win!';
+        winSpan.textContent = wins;
+        loseSpan.textContent = lose;
+        drawSpan.textContent = draws;
+        perSpan.textContent = `${(wins / total) * 100}%`;
+
+    } else if (userChoice === 'scissor' && computerChoice === 'paper'){
+        wins++;
+        total++;
+
+        resultSpan.textContent = 'Win!';
+        winSpan.textContent = wins;
+        loseSpan.textContent = lose;
+        drawSpan.textContent = draws;
+        perSpan.textContent = `${(wins / total) * 100}%`;
+    
+    } else if (userChoice === 'paper' && computerChoice === 'rock'){
+        wins++;
+        total++;
+
+        resultSpan.textContent = 'Win!';
+        winSpan.textContent = wins;
+        loseSpan.textContent = lose;
+        drawSpan.textContent = draws;
+        perSpan.textContent = `${(wins / total) * 100}%`;
+    
+    }
+    //lose
+    if (userChoice === 'rock' && computerChoice === 'paper'){
+        lose++;
+        total++;
+
+        resultSpan.textContent = 'Lose!';
+        winSpan.textContent = wins;
+        loseSpan.textContent = lose;
+        drawSpan.textContent = draws;
+        perSpan.textContent = `${(wins / total) * 100}%`;
+
+    } else if (userChoice === 'paper' && computerChoice === 'scissor'){
+        lose++;
+        total++;
+
+        resultSpan.textContent = 'Lose!';
+        winSpan.textContent = wins;
+        loseSpan.textContent = lose;
+        drawSpan.textContent = draws;
+        perSpan.textContent = `${(wins / total) * 100}%`;
+    } else if (userChoice === 'scissor' && computerChoice === 'rock'){
+        lose++;
+        total++;
+
+        resultSpan.textContent = 'Lose!';
+        winSpan.textContent = wins;
+        loseSpan.textContent = lose;
+        drawSpan.textContent = draws;
+        perSpan.textContent = `${(wins / total) * 100}%`;
+    
+    }
+   
+});
---
+++ utils.js
@@ -0,0 +1,26 @@
+
+export function gameFun(){
+    const randomNumber = Math.ceil(Math.random() * 3);
+    expect.equal(typeof actual, 'string');
+    let computerChoice;
+
+    if (randomNumber === 1){
+        computerChoice = 'rock';
+    }
+    if (randomNumber === 2){
+        computerChoice = 'paper';
+    }
+    if (randomNumber === 3){
+        computerChoice = 'scissor';
+    }
+    return computerChoice;
+}
+
+export function doUserWin(userChoice, value){
+    return userChoice;
+
+}
+
+
+
+//return userChoice === value;
