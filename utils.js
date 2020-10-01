f0ef7103b884b19639c18fc5342e4433121f0ab6
ryanleviathan <ryan.m.carreras@gmail.com>
2020-10-01 14:46:02 -0700

    all functions functioning like functioneers

--- utils.js
+++ utils.js
@@ -1,57 +1,38 @@
 
-export function gameFun(){
+export function gameFun() {
     const randomNumber = Math.ceil(Math.random() * 3);
-    expect.equal(typeof actual, 'string');
     let computerChoice;
 
-    if (randomNumber === 1){
-        computerChoice = 'rock';
-    }
-    if (randomNumber === 2){
-        computerChoice = 'paper';
-    }
-    if (randomNumber === 3){
-        computerChoice = 'scissor';
-    }
+    if (randomNumber === 1) computerChoice = 'rock';
+    if (randomNumber === 2) computerChoice = 'paper';
+    if (randomNumber === 3) computerChoice = 'scissor';
+    
     return computerChoice;
 }
 
-export function doesUserWin(userChoice, computerChoice){
+
+export function doesUserWin(userChoice) {
     //draws
+    const computerChoice = gameFun();
+
     if (userChoice === computerChoice){
-    draw++;
-    drawSpan.textContent = draw;
+        return 'draw';
     //wins
-    } else if (userChoice === 'rock' && computerChoice === 'paper'){
-        win++;
-        winSpan.textContent = wins;
-
+    } else if (userChoice === 'rock' && computerChoice === 'scissor'){
+        return 'win';
     } else if (userChoice === 'paper' && computerChoice === 'rock'){
-        win++;
-        winSpan.textContent = wins;
-
+        return 'win';
     } else if (userChoice === 'scissor' && computerChoice === 'paper'){
-        win++;
-        winSpan.textContent = wins;
+        return 'win';
     // loses
     } else if (userChoice === 'rock' && computerChoice === 'paper'){
-        lose++;
-        loseSpan.textContent = lose;
+        return 'lose';
     } else if (userChoice === 'paper' && computerChoice === 'scissor'){
-        lose++;
-        loseSpan.textContent = lose;
+        return 'lose';
     } else if (userChoice === 'scissor' && computerChoice === 'rock'){
-        lose++;
-        loseSpan.textContent = lose;
+        return 'lose';
     }
-    
-    
-    
-    
 }
-
-}
-
 
 
 
