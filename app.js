dc70708967d4ac495921e001c48859a484ab6385
DonnyLVu <donnyvu897@gmail.com>
2020-10-01 19:09:04 -0700

    added images

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const winSpan = document.querySelector('#win-span');
 const loseSpan = document.querySelector('#lose-span');
 const resultSpan = document.querySelector('#result-span');
 const drawSpan = document.querySelector('#draw-span');
+const resetButton = document.querySelector('#reset-button');
+const resetSpan = document.querySelector('#reset-span');
 // initialize state
 
 let win = 0;
 let lose = 0;
 let draw = 0;
+let resets = 0;
  
 // set event listeners to update state and DOM
 
@@ -20,12 +23,37 @@ button.addEventListener('click', () => {
     const radioButton = document.querySelector(':checked');
     const userChoice = radioButton.value;
     const results = doesUserWin(userChoice);
-
     winSpan.textContent = win;
     loseSpan.textContent = lose;
     drawSpan.textContent = draw;
+    resetSpan.textContent = resets;
+    
+
      
-    if (results === 'win') win++;
-    if (results === 'lose') lose++;
-    if (results === 'draw') draw++;
+    if (results === 'win') win++; resultSpan.textContent = 'Win!';
+
+    if (results === 'lose') lose++; resultSpan.textContent = 'Lose!';
+
+    if (results === 'draw') draw++; resultSpan.textContent = 'Tie!';
+
+    
+});
+
+resetButton.addEventListener('click', ()=>{
+    
+    if (win > 0){
+        win = 0;
+    }
+    if (lose > 0){
+        lose = 0;
+    }
+    
+    if (draw > 0){
+        draw = 0;
+    }
+
+    resets++;
+    resetSpan.textContent = resets;
+    
+   
 });
